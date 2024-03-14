d748723f20a0d49eeff6e25687d7859041964f04
krish <61118591+krish717@users.noreply.github.com>
2024-03-14 06:58:38 +0000

    conversion changes

---
+++ js_basics/conversionOperation.js
@@ -0,0 +1,14 @@
+let score = 52;
+console.log(typeof(score)); // this gives u NUMBER as output
+
+let scored = '85';
+console.log(typeof(scored)); // this gives u STRING  as output
+
+//string to number conversion
+console.log(typeof(Number(scored))); // in scored we store string but we convert here into numbr so output is number
+
+
+// "87" => 33
+// "jhf" => NAN
+//true = 1
+// false = 0
