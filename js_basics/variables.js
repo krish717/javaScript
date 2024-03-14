a74584f7ee7f5cfe1b1e2d629f28b5f762314f23
krish <61118591+krish717@users.noreply.github.com>
2024-03-14 06:31:12 +0000

    container config

--- js_basics/variables.js
+++ js_basics/variables.js
@@ -1,5 +1,5 @@
 const age = 23; //once u declared u can't change this
 var name = "krish";
 let mobile = 9773827637;
-
+// prefer not to use var bcz of issu in block scope and functional scope
 console.log([age,name,surname,mobile]);
