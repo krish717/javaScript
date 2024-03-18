6d427c3cb0c82238c96a82555b2c2740dce30881
krish <61118591+krish717@users.noreply.github.com>
2024-03-18 15:15:48 +0000

    config

---
+++ js_basics/string.js
@@ -0,0 +1,23 @@
+const firstName = 'krish';
+const lastName = 'Gupta';
+//concatenating
+// console.log(firstName+lastName);
+
+// but the correct to do concat use backtis
+// console.log(`hello guys my name is ${firstName} and last name is  ${lastName}`);
+
+
+//to declare the string
+const gameName = new String('      pubg');
+// console.log(gameName);
+
+
+//to check length of string
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('b'));
+console.log(gameName.trim());
+
+console.log(gameName.replace('u','a'));
+console.log(gameName.includes('u'));
