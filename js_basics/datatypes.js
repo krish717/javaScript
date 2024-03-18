c1bf59bfbdff8d9b804920010b49cf5d3a4c4ae9
krish <61118591+krish717@users.noreply.github.com>
2024-03-18 14:21:46 +0000

    config

--- js_basics/datatypes.js
+++ js_basics/datatypes.js
@@ -1,2 +1,37 @@
 "use strict" //this line means trat all js code as newwer version
 //code readeabilty should be high
+
+
+//two types of datatypes 1-> primitive 2-> NOn-primitive
+
+// 7 types of primitive
+//string boolean number null undefined symbol BigInt
+
+
+// JavaScript is a dynamically typed language. This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime.
+
+// For example, in JavaScript, you can do the following:
+
+// let x = 10; // x is now a number
+// x = "Hello"; // x is now a string
+// x = true; // x is now a boolean
+// On the other hand, statically typed languages require you to declare the variable's type explicitly, and the type checking is done at compile-time, before the code is executed.
+
+
+//non primitive=== reference 
+//Array objects functions
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
