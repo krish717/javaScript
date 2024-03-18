f45b501f1cb804397c025fdbc0306d904c9b9f71
krish <61118591+krish717@users.noreply.github.com>
2024-03-18 16:10:27 +0000

    config

---
+++ js_basics/num_and_math.js
@@ -0,0 +1,35 @@
+// const score = 400;
+
+// const balance = new Number(100.987);
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+// console.log(balance.toPrecision(3));
+
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//##############################       MATHS
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(5.4)); //top
+// console.log(Math.round(5.1)); //bottom
+
+console.log(Math.min(4,8,9,2));
+console.log(Math.max(4,8,9,2));
+
+console.log(Math.random()); //its give u the value of b/w 0 to 1
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(min-max+1))+min);
