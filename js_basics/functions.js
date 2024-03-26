3bf523f7d24a6c875a7719234be73d4079b1b506
krish <61118591+krish717@users.noreply.github.com>
2024-03-26 07:25:46 +0000

    config function

---
+++ js_basics/functions.js
@@ -0,0 +1,59 @@
+//function defination
+// function myname(){
+//      console.log("K");
+//      console.log("R");
+//      console.log("I");
+//      console.log("S");
+//      console.log("H");
+// }
+
+//function call
+// myname();
+
+// function addTwoNum(num1,num2){
+//     console.log(num1+num2);
+// }
+// function addTwoNum(num1,num2){
+//     let result = num1+num2;
+//     return result;
+// }
+// const addfun = addTwoNum(2,56);
+// console.log(addfun);
+
+// function loginusermsg(username="uma"){
+//     if(!username){
+//         console.log("please fill the details");
+//         return;
+//     }
+//     console.log(`${username} logged in`);
+// }
+// console.log(loginusermsg());
+
+
+
+///next function video
+//use rest operator when ever a use user add to cart so can't how much product add into an cart so calculare to price is a big problem so we use here rest operstor ... that is add into an array all value
+// function calculateCartPrice(...num){
+//           return num;
+// }
+// console.log(calculateCartPrice(55,63,53,75,7896,522));
+
+
+//lets talk about function with object
+// const user = {
+//     name : "krish",
+//     price:85
+// }
+
+// function funwithobject(anyobject){
+//          console.log(`hii ${anyobject.name} and your price is ${anyobject.price}`);
+// }
+// funwithobject(user);
+
+
+// lets talk about functions with array
+// const user = ["krish","heena","rahul","abhi"];
+// function functionwitharray(anyarray){
+//       console.log(anyarray[1]);
+// }
+// functionwitharray(user);
