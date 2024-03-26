3e68c9996a8f77bd3241d9eeae6d285bc7a71aea
krish <61118591+krish717@users.noreply.github.com>
2024-03-26 08:59:03 +0000

    config scope

---
+++ js_basics/scope.js
@@ -0,0 +1,21 @@
+// Global Scope:
+
+// Variables declared outside of any function have global scope.
+
+// They can be accessed from anywhere in the code, including inside functions.
+// const a  = 45;
+// if(true){
+//     console.log(a);
+// }
+
+// Local Scope:
+
+// Variables declared inside a function have local scope.
+
+// They can only be accessed within the function where they are declared.
+
+// if(true){
+//     const b = 59;
+// }
+
+// console.log(b); //it give u the error b is not defined
