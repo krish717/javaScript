c97031987e063419966efbf16f591e88dcf0658e
krish <61118591+krish717@users.noreply.github.com>
2024-03-28 03:43:10 +0000

    config loop

---
+++ js_basics/loop.js
@@ -0,0 +1,17 @@
+//loop
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+
+//break ans continue;
+for(let i=0;i<=10;i++){
+    if(i==5) break;
+        console.log(i);
+}
+
+
+for(let i=0;i<=10;i++){
+    if(i==5) continue;
+    console.log(i);
+}
