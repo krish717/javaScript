548f8c2d8497f3f7f436b9ba13497b97cba268dc
krish <61118591+krish717@users.noreply.github.com>
2024-03-28 11:36:38 +0000

    config loop

--- js_basics/loop.js
+++ js_basics/loop.js
@@ -5,13 +5,78 @@
 
 
 //break ans continue;
-for(let i=0;i<=10;i++){
-    if(i==5) break;
-        console.log(i);
+// for(let i=0;i<=10;i++){
+//     if(i==5) break;
+//         console.log(i);
+// }
+
+
+// for(let i=0;i<=10;i++){
+//     if(i==5) continue;
+//     console.log(i);
+// }
+
+//map is an object that holds the key value pairs
+
+// const map = new Map();
+// map.set('In',"India");
+// map.set('Fr',"France");
+// map.set('Ch',"China");
+
+// for(const [key,value] of map){
+//     console.log(key, '=>', value);
+// }
+
+// const user = [1,2,3,4,5,56,7,8,9];
+// for(const num of user) console.log(num);
+
+
+const myobject = {
+    game1 :"Pubg",
+    gam2 : "freefire"
 }
 
 
-for(let i=0;i<=10;i++){
-    if(i==5) continue;
-    console.log(i);
-}
+//for of not iterate on object 
+//for in loop is ised for object and also other work
+
+// for(const key in myobject){
+//     console.log(`${key} and value is ${myobject[key]}`);
+// }
+
+
+
+//for each loop
+const coding = ["php","js","c++","c","java"];
+// coding.forEach(function (val){
+//     console.log(val);
+// })
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// })
+
+// coding.forEach( (val,index,arr) => {
+//     console.log(val,index,arr);
+// })
+
+
+const mycoding = [
+    {
+        languageName:"javaScript",
+        fileName : "js"
+    },
+    {
+        languageName:"C++",
+        fileName : "c++"
+    },
+    {
+        languageName:"java",
+        fileName : "java"
+    }
+]
+
+
+mycoding.forEach((item) => {
+    console.log(item.languageName);
+})
